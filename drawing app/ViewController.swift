d2062a410f3e433f67dc6ad4fa40e212e57937fb
Raphaël Reiter <raphaelreiter@Raphaels-iMac.local>
2017-06-26 20:55:05 +0200

    we can draw a line

--- drawing app/ViewController.swift
+++ drawing app/ViewController.swift
@@ -10,16 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var lastPoint = CGPoint.zero
+    var swiped = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        swiped = false
+        
+        if let touch = touches.first as UITouch! {
+            
+            lastPoint = touch.location(in: self.view)
+            
+        }
     }
-
+    
+    func drawLine(_ fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        
+        let context = UIGraphicsGetCurrentContext()
+        imageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width , height: view.frame.size.height))
+        
+        context?.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+        
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(5)
+        context?.setStrokeColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+        context?.setBlendMode(CGBlendMode.normal)
+        
+        context?.strokePath()
+        
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        
+    }
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        swiped = true
+        
+        if let touch = touches.first as UITouch! {
+            
+            let currentPoint = touch.location(in: view)
+            drawLine(lastPoint, toPoint: currentPoint)
+            
+            lastPoint = currentPoint
+        }
+    }
+    
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if !swiped {
+            
+            drawLine(lastPoint, toPoint: lastPoint)
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
